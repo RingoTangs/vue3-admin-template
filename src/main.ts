32d0ca05f77cafeb7cbcf359503b7222c485104e
RingoTangs <1466637477@qq.com>
2023-06-20 21:39:26 +0800

    SvgIcon

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import App from './App.vue'
 import router from './router'
 import i18n from './i18n'
 import store from './store'
+import 'virtual:svg-icons-register'
 
 // prettier-ignore
 createApp(App)
--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,21 @@ import path from 'path'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+import { createSvgIconsPlugin } from 'vite-plugin-svg-icons'
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [
         vue(),
+
+        // https://github.com/vbenjs/vite-plugin-svg-icons
+        createSvgIconsPlugin({
+            // 指定 SVG图标 保存的文件夹路径
+            iconDirs: [path.resolve(process.cwd(), 'src/assets/icons')],
+            // 指定 使用svg图标的格式
+            symbolId: 'icon-[dir]-[name]',
+        }),
+
         AutoImport({
             resolvers: [ElementPlusResolver()],
         }),
