1c5a768677366cc9f524fd18b99bf1d3060620c5
RingoTangs <1466637477@qq.com>
2023-06-20 17:44:07 +0800

    element-plus

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,7 +1,15 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 import type { RouteRecordRaw } from 'vue-router'
 
-const constantRoutes: Readonly<RouteRecordRaw[]> = []
+const constantRoutes: Readonly<RouteRecordRaw[]> = [
+    {
+        path: '/login',
+        component: import('@/views/login/ViewsLogin.vue'),
+        meta: {
+            hidden: true,
+        },
+    },
+]
 
 export default createRouter({
     routes: constantRoutes,
---
+++ src/views/login/ViewsLogin.vue
@@ -0,0 +1,5 @@
+<template>
+    <div id="views_login" class="w-full min-h-full"></div>
+</template>
+
+<script setup lang="ts"></script>
