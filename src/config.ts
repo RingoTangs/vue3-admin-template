63d279457078634e3e5246c2aec2357dddb75491
RingoTangs <1466637477@qq.com>
2023-06-22 22:08:07 +0800

    添加项目名字

--- src/config.ts
+++ src/config.ts
@@ -1,11 +1,9 @@
-const isDev: boolean = import.meta.env.DEV
-const isProd: boolean = import.meta.env.PROD
-const mode: string = import.meta.env.MODE
-const apiBaseUrl: string = import.meta.env.VITE_APP_API_BASE_URL
+export const isDev: boolean = import.meta.env.DEV
+export const isProd: boolean = import.meta.env.PROD
+export const mode: string = import.meta.env.MODE
 
-export default {
-    isProd,
-    isDev,
-    mode,
-    apiBaseUrl,
-}
+// 后端 API 地址, 配置 axios baseUrl 使用
+export const apiBaseUrl: string = import.meta.env.VITE_APP_API_BASE_URL
+
+// 项目名字
+export const documentTitle: string = import.meta.env.VITE_APP_PROJECT_TITLE
