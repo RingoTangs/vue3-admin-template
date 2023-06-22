69d4b3473f9471f79cb4195440854db2576e650a
RingoTangs <1466637477@qq.com>
2023-06-22 18:12:16 +0800

    mock

--- mock/index.cjs
+++ mock/index.cjs
@@ -1,8 +1,13 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 
-const a = require('./user.cjs')
-console.log(a)
+const user = require('./user.cjs')
 
 const app = express()
+app.use(bodyParser.json()) // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+
+user(app)
+
 const port = 3000
-app.listen(port, () => console.log('mock server starting. port: ' + port))
+app.listen(port, () => console.log(`mock server starting. port: ${port}.`))
--- mock/user.cjs
+++ mock/user.cjs
@@ -1,2 +1,52 @@
-const a = 1
-module.exports = a
+const tokens = {
+    admin: {
+        token: 'admin-token',
+    },
+    editor: {
+        token: 'editor-token',
+    },
+}
+
+const users = {
+    'admin-token': {
+        roles: ['admin'],
+        introduction: 'I am a super administrator',
+        avatar: 'https://wpimg.wallstcn.com/f778738c-e4f8-4870-b634-56703b4acafe.gif',
+        name: 'Super Admin',
+    },
+    'editor-token': {
+        roles: ['editor'],
+        introduction: 'I am an editor',
+        avatar: 'https://wpimg.wallstcn.com/f778738c-e4f8-4870-b634-56703b4acafe.gif',
+        name: 'Normal Editor',
+    },
+}
+
+module.exports = function (app) {
+    // user login
+    app.post('/vue3-admin-template/user/login', (req, resp) => {
+        const { username } = req.body
+        const token = tokens[username]
+        if (!token) {
+            resp.json({ code: 500, message: 'Account and password are incorrect.' })
+            return
+        }
+        resp.json({ code: 200, message: 'Login Success.', data: token })
+    })
+
+    // get user info
+    app.get('/vue3-admin-template/user/info', (req, resp) => {
+        const { token } = req.query
+        const info = users[token]
+        if (!info) {
+            resp.json({ code: 500, message: 'Login failed, unable to get user details.' })
+            return
+        }
+        resp.json({ code: 200, message: 'Get User Info Success.', data: info })
+    })
+
+    // user logout
+    app.post('/vue3-admin-template/user/logout', (req, resp) => {
+        resp.json({ code: 200, message: 'Logout Success.' })
+    })
+}
