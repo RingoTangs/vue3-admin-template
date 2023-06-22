8ae3a91d4ab9b3932eddaab0112f72b45e67b3f1
RingoTangs <1466637477@qq.com>
2023-06-22 23:09:50 +0800

    update permission

--- src/permission.ts
+++ src/permission.ts
@@ -2,8 +2,10 @@ import router from '@/router'
 import NProgress from 'nprogress'
 import 'nprogress/nprogress.css'
 import { getFullPageTitle } from '@/config'
+import { default as pinia, useTokenStore } from '@/store'
 
 const whiteList = ['/login'] // no redirect whitelist
+const tokenStore = useTokenStore(pinia)
 
 router.beforeEach((to, from, next) => {
     // start progress bar
