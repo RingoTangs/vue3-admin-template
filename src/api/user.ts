6b3622cca8d2cdc41158710cc1392416fc0e9ef6
RingoTangs <1466637477@qq.com>
2023-06-23 13:46:42 +0800

    API

--- src/api/user.ts
+++ src/api/user.ts
@@ -1,21 +1,23 @@
 import request from '@/utils/request'
 
+// User Login API
 export type LoginRequest = { username: string; password: string }
 export function login(data: LoginRequest) {
     return request<string, LoginRequest>({ url: '/user/login', method: 'post', data })
 }
 
-// export function getUserInfo(token: string) {
-//     return request<CommonResponse<string>>({
-//         url: '/vue3-admin-template/user/info',
-//         method: 'get',
-//         params: { token },
-//     })
-// }
+// Get User Info API
+export type GetUserInfoResponse = {
+    name: string
+    avatar: string
+    introduction: string
+    roles: string[]
+}
+export function getUserInfo(token: string) {
+    return request<GetUserInfoResponse>({ url: '/user/info', method: 'get', params: { token } })
+}
 
-// export function logout() {
-//     return request({
-//         url: '/vue-admin-template/user/logout',
-//         method: 'post',
-//     })
-// }
+// User Logout API
+export function logout() {
+    return request<void>({ url: '/user/logout', method: 'post' })
+}
