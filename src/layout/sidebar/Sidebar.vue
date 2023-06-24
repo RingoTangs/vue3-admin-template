4796d564efe2e37f3918e907a7d6bfb9a5693ef3
RingoTangs <1466637477@qq.com>
2023-06-24 19:09:21 +0800

    Sidebar

--- src/layout/sidebar/Sidebar.vue
+++ src/layout/sidebar/Sidebar.vue
@@ -1,12 +1,22 @@
 <template>
-    <div class="w-full h-full sidebar">
-        <el-scrollbar :wrap-style="scrollbarStyle" :view-style="scrollbarStyle">
-        </el-scrollbar>
-    </div>
+    <el-scrollbar :wrap-style="scrollbarStyle" :view-style="scrollbarStyle">
+        <!-- <div v-for="i in 1000" :key="i">1</div> -->
+        <el-menu text-color="#707070">
+            <el-menu-item v-for="i in 100" :key="i">
+                <el-icon>aa</el-icon>
+                <span>Navigator Three</span>
+            </el-menu-item>
+        </el-menu>
+    </el-scrollbar>
 </template>
 
 <script setup lang="ts">
 const scrollbarStyle = { maxHeight: '100%' }
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+:deep(.el-menu-item) {
+    @apply text-xl font-semibold hover:bg-[#695cfe] hover:text-white;
+    @apply transition-all  mx-5 rounded-lg;
+}
+</style>
