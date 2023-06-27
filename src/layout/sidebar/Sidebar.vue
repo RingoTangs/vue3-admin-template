d5d65d5d67c2742329979e0daae4479680debbc0
RingoTangs <1466637477@qq.com>
2023-06-27 20:48:08 +0800

    SidebarItem

--- src/layout/sidebar/Sidebar.vue
+++ src/layout/sidebar/Sidebar.vue
@@ -1,11 +1,18 @@
 <template>
     <el-menu text-color="#707070" background-color="#f1f5f9" class="sidebar">
-        <Link to="/dashboard"></Link>
+        <SidebarItem to="/dashboard">
+            <template #prefix>Icon</template>
+            Dashboard
+        </SidebarItem>
+        <SidebarItem to="/example/table">
+            <template #prefix>Icon</template>
+            Table
+        </SidebarItem>
     </el-menu>
 </template>
 
 <script setup lang="ts">
-import Link from './SidebarItemLink.vue'
+import SidebarItem from './SidebarItem.vue'
 </script>
 
 <style lang="scss" scoped></style>
--- src/layout/sidebar/SidebarItem.vue
+++ src/layout/sidebar/SidebarItem.vue
@@ -1,12 +1,39 @@
 <template>
-    <div class="sidebar_item"></div>
+    <router-link :to="to" v-if="show" class="block mx-4 my-3">
+        <el-menu-item :class="{ active: route.path === to }">
+            <span class="mr-3 prefix">
+                <slot name="prefix"></slot>
+            </span>
+            <span>
+                <slot></slot>
+            </span>
+        </el-menu-item>
+    </router-link>
 </template>
 
 <script setup lang="ts">
-type Link = { type: 'link', to: string }
-type Group = { type: 'group', }
+import { computed } from 'vue'
+import { useRouter, useRoute } from 'vue-router'
 
-// defineProps<{  }>()
+const props = defineProps<{ to: string }>()
+
+const routes = useRouter().getRoutes()
+const route = useRoute()
+
+const show = computed(() => {
+    const targetRoute = routes.find((d) => d.path === props.to)
+    if (!targetRoute) return false
+    return !targetRoute.meta.hidden
+})
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+:deep(.el-menu-item) {
+    @apply text-lg font-semibold hover:bg-[#695cfe] hover:text-white;
+    @apply transition-all rounded-lg duration-700;
+}
+
+.active {
+    @apply bg-[#695cfe] text-white;
+}
+</style>
