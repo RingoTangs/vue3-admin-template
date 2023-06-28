0bc87fcf3ab4d607d0e3830318b07f0454774531
RingoTangs <1466637477@qq.com>
2023-06-28 17:17:08 +0800

    Sidebar dashboard

--- src/layout/sidebar/Sidebar.vue
+++ src/layout/sidebar/Sidebar.vue
@@ -1,7 +1,10 @@
 <template>
     <el-menu text-color="#707070" background-color="#f1f5f9" class="sidebar">
+        <!-- 首页/dashboard -->
         <SidebarItem to="/dashboard">
-            <template #prefix>Icon</template>
+            <template #prefix>
+                <SvgIcon name="dashboard" class="w-8 h-8"></SvgIcon>
+            </template>
             {{ t('dashboardPage.title') }}
         </SidebarItem>
     </el-menu>
@@ -10,6 +13,7 @@
 <script setup lang="ts">
 import SidebarItem from './SidebarItem.vue'
 import { useI18n } from 'vue-i18n'
+import SvgIcon from '@/components/svg-icon/SvgIcon.vue'
 
 const { t } = useI18n()
 </script>
